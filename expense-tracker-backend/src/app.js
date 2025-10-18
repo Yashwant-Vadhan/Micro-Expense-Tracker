30a5f7173a0ede919bafcec7b343135c2207a2aa
Yashwant-Vadhan <yashuvadhan8231@gmail.com>
2025-10-18 19:19:03 +0530

    Undone Altered app.js (moved use(express.son) above)

--- expense-tracker-backend/src/app.js
+++ expense-tracker-backend/src/app.js
@@ -5,7 +5,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
-app.use(express.json());
+
 
 const userRoutes = require('./routes/userRoutes');
 const sourceRoutes = require('./routes/sourceRoutes');
@@ -22,7 +22,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
-
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
